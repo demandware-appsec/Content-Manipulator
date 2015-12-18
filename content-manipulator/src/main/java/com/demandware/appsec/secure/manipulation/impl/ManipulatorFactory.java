3d232a1da4130da8a1ef9d89e681df8cff75a023
Chris Smith <csmith@demandware.com>
2015-12-18 13:39:45 -0500

    Updated License and exposed library integration point
    Officially on Apache 2.0
    Also exposed the registration function on ManipulatorFactory to allow subclassing via registration.

--- content-manipulator/src/main/java/com/demandware/appsec/secure/manipulation/impl/ManipulatorFactory.java
+++ content-manipulator/src/main/java/com/demandware/appsec/secure/manipulation/impl/ManipulatorFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Demandware Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.demandware.appsec.secure.manipulation.impl;
 
 import java.util.HashMap;
@@ -17,6 +32,14 @@ public class ManipulatorFactory
         {
             this.manipulatorMap.put( t, t.getManipulator() );
         }
+    }
+    
+    public static void registerManipulationTypes( IManipulationType... types ){
+    	for( int i = 0; i < types.length; i++ )
+    	{
+    		IManipulationType type = types[i];
+    		instance.manipulatorMap.put( type, type.getManipulator() );
+    	}
     }
 
     public static AbstractManipulator getManipulator( IManipulationType type )
--- content-manipulator/src/test/java/com/demandware/appsec/secure/manipulation/impl/XMLManipulatorTest.java
+++ content-manipulator/src/test/java/com/demandware/appsec/secure/manipulation/impl/XMLManipulatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Demandware Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.demandware.appsec.secure.manipulation.impl;
 
 import static org.junit.Assert.assertEquals;
@@ -14,7 +29,7 @@ import com.demandware.appsec.secure.manipulation.impl.ManipulatorFactory;
 import com.demandware.appsec.secure.manipulation.impl.XMLManipulator;
 import com.demandware.appsec.secure.manipulation.impl.XMLManipulator.XMLManipulatorOption;
 
-public class _XMLManipulator
+public class XMLManipulatorTest
 {
     private final XMLManipulator comMan = (XMLManipulator) ManipulatorFactory
         .getManipulator( DefaultManipulationType.XML_COMMENT_MANIPULATOR );
